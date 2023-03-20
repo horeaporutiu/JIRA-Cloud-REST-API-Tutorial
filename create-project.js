694c24c155a72c8bab608ddc5fbdc0cd3bdd622a
Horea <horeaporutiu@gmail.com>
2023-03-20 15:43:23 -0700

    split create issue into sep file

--- create-project.js
+++ create-project.js
@@ -49,57 +49,6 @@ async function createProject(projectName) {
 
 }
 
-//creates an issue in Jira Cloud using REST API 
-async function createIssue(projectKey, issueType, summary, description) {
-
-  try {
-
-    const baseUrl = 'https://' + domain + '.atlassian.net';
-
-    const data = {
-      fields: {
-        project: { key: projectKey },
-        summary: summary,
-        description: description,
-        issuetype: { name: issueType }
-      }
-    };
-    const config = {
-      headers: { 'Content-Type': 'application/json' },
-      auth: auth
-    };
-    const response = await axios.post(`${baseUrl}/rest/api/2/issue`, data, config);
-    return response.data.key;
-
-  } catch (error) {
-    console.log('error: ')
-    console.log(error.response.data.errors)
-  }
-}
-
-//creates an issue in Jira Cloud using REST API 
-async function getUsers() {
-
-  try {
-
-    const baseUrl = 'https://' + domain + '.atlassian.net';
-
-    const config = {
-      method: 'get',
-      url: baseUrl + '/rest/api/2/users',
-      headers: { 'Content-Type': 'application/json' },
-      auth: auth
-    };
-    const response = await axios.request(config);
-    console.log(response)
-    return response.data;
-
-  } catch (error) {
-    console.log('error: ')
-    console.log(error.response.data.errors)
-  }
-}
-
 const createProjectCaller = async () => {
 
   const projectName = process.env.PROJECT_NAME
