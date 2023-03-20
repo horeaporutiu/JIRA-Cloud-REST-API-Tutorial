cee20ad9f7f6d14b7d8491e852a9a9e018467107
Horea <horeaporutiu@gmail.com>
2023-03-19 21:36:09 -0700

    first commit

--- create-project.js
+++ create-project.js
@@ -6,76 +6,76 @@ const password = process.env.ATLASSIAN_API_KEY
 const domain = process.env.DOMAIN
 
 const auth = {
-	username: username,
-	password: password
+  username: username,
+  password: password
 };
 
 async function createProject(projectName) {
 
-	try {
+  try {
 
 
-		const leadAccountID = process.env.LEAD_ACCT_ID
-		const baseUrl = 'https://' + domain + '.atlassian.net';
-		const projKey = process.env.PROJECT_KEY
+    const leadAccountID = process.env.LEAD_ACCT_ID
+    const baseUrl = 'https://' + domain + '.atlassian.net';
+    const projKey = process.env.PROJECT_KEY
 
-		const data = {
-			key: projKey,
-			name: projectName,
-			projectTypeKey: 'software',
-			"leadAccountId": leadAccountID
-		};
-		const config = {
-			headers: { 'Content-Type': 'application/json' },
-			auth: auth
-		};
-		const response = await axios.post(`${baseUrl}/rest/api/3/project`, data, config);
-		console.log(response.data)
-		return response.data.key;
+    const data = {
+      key: projKey,
+      name: projectName,
+      projectTypeKey: 'software',
+      "leadAccountId": leadAccountID
+    };
+    const config = {
+      headers: { 'Content-Type': 'application/json' },
+      auth: auth
+    };
+    const response = await axios.post(`${baseUrl}/rest/api/3/project`, data, config);
+    console.log(response.data)
+    return response.data.key;
 
-	} catch (error) {
-		console.log('error: ')
-		console.log(error.response.data.errors)
-	}
+  } catch (error) {
+    console.log('error: ')
+    console.log(error.response.data.errors)
+  }
 
 }
 
 async function createIssue(projectKey, issueType, summary, description) {
 
-	try {
+  try {
 
-		const baseUrl = 'https://' + domain + '.atlassian.net';
+    const baseUrl = 'https://' + domain + '.atlassian.net';
 
-		const data = {
-			fields: {
-				project: { key: projectKey },
-				summary: summary,
-				description: description,
-				issuetype: { name: issueType }
-			}
-		};
-		const config = {
-			headers: { 'Content-Type': 'application/json' },
-			auth: auth
-		};
-		const response = await axios.post(`${baseUrl}/rest/api/2/issue`, data, config);
-		return response.data.key;
+    const data = {
+      fields: {
+        project: { key: projectKey },
+        summary: summary,
+        description: description,
+        issuetype: { name: issueType }
+      }
+    };
+    const config = {
+      headers: { 'Content-Type': 'application/json' },
+      auth: auth
+    };
+    const response = await axios.post(`${baseUrl}/rest/api/2/issue`, data, config);
+    return response.data.key;
 
-	} catch (error) {
-		console.log('error: ')
-		console.log(error.response.data.errors)
-	}
+  } catch (error) {
+    console.log('error: ')
+    console.log(error.response.data.errors)
+  }
 }
 
 const createProjectAndIssue = async () => {
-	const issueType = 'Task';
-	const summary = 'Horea Porutiu is awesome';
-	const description = 'super awesome!'
-	const projectName = process.env.PROJECT_NAME
-	const projectKey = await createProject(projectName);
-	console.log(`Created project with key: ${projectKey}`);
-	const issueKey = await createIssue(projectKey, issueType, summary, description);
-	console.log(`Created issue with key: ${issueKey}`);
+  const issueType = 'Task';
+  const summary = 'Horea Porutiu is awesome';
+  const description = 'super awesome!'
+  const projectName = process.env.PROJECT_NAME
+  const projectKey = await createProject(projectName);
+  console.log(`Created project with key: ${projectKey}`);
+  const issueKey = await createIssue(projectKey, issueType, summary, description);
+  console.log(`Created issue with key: ${issueKey}`);
 }
 
 createProjectAndIssue();
